2c1fce3fc1eafc66368dcec2e823dbd1317e6e5e
OlgaResh1 <gamogenesis@yandex.ru>
2024-02-15 19:06:53 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,56 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const invalidSymbol rune = -1
+
+func isDigit(sym rune) bool {
+	return (sym >= '0' && sym <= '9') // strconv.Atoi(string(sym))?
+}
+func isSlash(sym rune) bool {
+	return sym == '\\'
+}
+
+func Unpack(input string) (string, error) {
+
+	result := strings.Builder{}
+	lastSym := invalidSymbol
+	slash := false
+
+	for _, sym := range input {
+		if slash {
+			if !isDigit(sym) && !isSlash(sym) {
+				return "", ErrInvalidString
+			}
+			slash = false
+			lastSym = sym
+			continue
+		}
+
+		if isDigit(sym) {
+			if lastSym == invalidSymbol {
+				return "", ErrInvalidString
+			}
+			result.WriteString(strings.Repeat(string(lastSym), int(sym-'0')))
+			lastSym = invalidSymbol
+			continue
+		}
+
+		if isSlash(sym) {
+			slash = true
+		}
+
+		if lastSym != invalidSymbol {
+			result.WriteRune(lastSym)
+		}
+		lastSym = sym
+	}
+	if lastSym != invalidSymbol {
+		result.WriteRune(lastSym)
+	}
+	return result.String(), nil
 }
