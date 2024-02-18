d358ad7a03834187bfda116e1997eb84b7e22644
OlgaResh1 <gamogenesis@yandex.ru>
2024-02-18 14:08:10 +0300

    hw03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(input string) []string {
+	splitSlice := strings.Fields(input)
+
+	freqMap := make(map[string]int)
+
+	for _, word := range splitSlice {
+		freqMap[word]++
+	}
+
+	uniqSlice := make([]string, len(freqMap))
+	i := 0
+	for word := range freqMap {
+		uniqSlice[i] = word
+		i++
+	}
+	sort.Slice(uniqSlice, func(i, j int) bool {
+		f1 := freqMap[uniqSlice[i]]
+		f2 := freqMap[uniqSlice[j]]
+		if f1 == f2 {
+			return uniqSlice[i] < uniqSlice[j]
+		}
+		return f1 > f2
+	})
+
+	if len(uniqSlice) < 10 {
+		return uniqSlice
+	}
+	return uniqSlice[:10]
 }
