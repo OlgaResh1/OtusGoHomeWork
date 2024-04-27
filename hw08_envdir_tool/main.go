00506d4465e07869a5183469fa12c2f40acbaae0
OlgaResh1 <gamogenesis@yandex.ru>
2024-04-27 23:19:29 +0300

    HW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Println("Usage:	go-envdir pathToDir process arguments ")
+		os.Exit(1)
+	}
+	env, err := ReadDir(args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	returnCode := RunCmd(args[2:], env)
+	os.Exit(returnCode)
 }
